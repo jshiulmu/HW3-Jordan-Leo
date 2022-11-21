220bb98c7aad8aa36d20f35a67f5cb6a8410d6f9
jshiulmu <jshiuwork@gmail.com>
2022-11-21 10:57:46 -0800

    Deletion working!

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -4,7 +4,8 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 //accessible VIA CONSOLE.FIREBASE.GOOGLE.COM (17JORDOSHIU@GMAIL.COM)
-//FIREBASE CONTENTS :https://console.firebase.google.com/u/0/project/blog-starter-app-jordan-base/firestore/data/~2Farticles~2F0CmxPTfc15XH33IJZTQ9
+//FIREBASE CONTENTS :
+//  https://console.firebase.google.com/u/0/project/blog-starter-app-jordan-base/firestore/data/~2Farticles~2F0CmxPTfc15XH33IJZTQ9
 const firebaseConfig = {
   apiKey: "AIzaSyCy8k-vRXTUfXsoQ3PqZBPUAEzyXv67GM0",
   authDomain: "blog-starter-app-jordan-base.firebaseapp.com",
