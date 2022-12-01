1de14f0a25ae992e1cd4324efa5fd62aa2e6f4a1
zleodai <awsomeleodai@gmail.com>
2022-11-30 17:24:52 -0800

    background
    done

--- src/components/ArticleEntry.js
+++ src/components/ArticleEntry.js
@@ -16,7 +16,7 @@ export default function ArticleEntry({ addArticle }) {
   }
 
   return (
-    <div>
+    <div className="Article_Box">
       <form onSubmit={submit}>
         {error && <p className="error">{error}</p>}
         Title
